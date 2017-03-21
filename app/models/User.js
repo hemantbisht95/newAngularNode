cf4cd2caedb33524a3f522d3e39cd15076487f0d
hemantbisht95 <bisht.hemant99@gmail.com>
2017-03-21 18:22:50 +0530

    Update User.js

--- app/models/User.js
+++ app/models/User.js
@@ -1,18 +1,21 @@
-
 var mongoose = require('mongoose');
 // var mongoosePages = require('mongoose-pages');
-var CommentModel = require("./comments");
+var StoryModel = require("./story");
 var Schema = mongoose.Schema,
 ObjectId = Schema.ObjectId;
+
 var Userschema = new Schema
 ({
     firstname:String,
     lastname:String,
     email:String,
-    password:String
+    password:String,
+    story:[{
+      type:ObjectId,
+      ref:"story"}]
 
 
 });
-// mongoosePages.skip(Userschema);
+
 var User = mongoose.model('User', Userschema);
 module.exports=User;
