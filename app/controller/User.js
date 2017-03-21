04fb60b7f6b7a421a2cebb6961e14ed232e5d2c4
hemantbisht95 <bisht.hemant99@gmail.com>
2017-03-21 18:21:54 +0530

    Update User.js

--- app/controller/User.js
+++ app/controller/User.js
@@ -1,6 +1,7 @@
 var UserModel = require("./../models/User");
 var jsonwebtoken = require('jsonwebtoken');
-
+var StoryModel = require("./../models/story");
+var CommentModel = require("./../models/comments");
 
 exports.getUser = function(req, res){
 UserModel.find({}).populate("story").exec(function(err, User){
@@ -14,7 +15,6 @@ UserModel.find({}).populate("story").exec(function(err, User){
 
 exports.auth=function(req,res){
 res.json({code:450,message:"awd"});
-
 }
 
 exports.addUser = function(req, res){
@@ -35,8 +35,9 @@ exports.getUserById = function(req, res){
        if(err){
        	      res.json({code: 400, message: "Error occurred"});
               }else{
+
 	                 res.json({code: 200, message: "records found",data:User});
-           }
+                   }
       })
 }
 
@@ -66,8 +67,8 @@ exports.regUser = function(req, res){
     var User = new UserModel(req.body);
     User.save(function (err){
       if (err){
-              res.json({code:400,message:"error"});
-              }else{
+            res.json({code:400,message:"error"});
+            }else{
                    res.json({code:200,message:"created",data:User});
               }
         })
@@ -94,17 +95,144 @@ exports.logUser = function(req, res){
              }else{
                   res.json({code:404,message:"email or passwod is wrong"});
             }
-      });
-  }
+         });
+       }
 
   exports.getuserProfile = function(req, res){
 
-    UserModel.findOne({'_id':req.user.id}).exec(function(err, user)
+    UserModel.findOne({'_id':req.user.id}).populate('story').exec(function(err, user)
       {
          if(user){
-                res.json({code: 200, message: "records found",data:user});
-                }else{
+            res.json({code: 200, message: "records found",data:user});
+             } else {
                    res.json({code: 400, message: "Error occurred"});
-               }
-         });
+           }
+      });
+   }
+
+   exports.getUserComment = function(req, res){
+
+    CommentModel.findById({'_id':req.params.id}).exec(function(err, user)
+       {
+          if(user){
+             res.json({code: 200, message: "records found",data:user});
+              } else {
+                    res.json({code: 400, message: "Error occurred"});
+            }
+       });
     }
+    exports.postComment = function(req, res){
+        var User = new   CommentModel(req.body);
+        console.log(req.body);
+        User.save(function (err){
+          if (err){
+                res.json({code:400,message:"error"});
+                }else{
+                       res.json({code:200,message:"created",data:User});
+                  }
+            })
+      }
+
+   exports.getStory = function(req, res)
+   {
+   StoryModel.find({}).sort('-created').populate("comment").exec(function(err, story)
+   {
+      if(err){
+         res.json({code: 400, message: "Error occurred"});
+       }
+     else{
+
+     res.json({code: 200, message: "records found",data:story});
+         }
+
+     })
+   }
+   exports.addStory = function(req, res) {
+     var story = new StoryModel(req.body);
+     story.save(function(err) {
+        UserModel.update({'_id': req.user.id}, {$push: {story: story}}, function(err, Story){
+       if (err) {
+         res.json({
+           code: 400,
+           message: "error"
+         });
+       } else {
+         res.json({
+           code: 200,
+           message: "created",
+           data: story
+         });
+
+       }
+     })
+   })
+ }
+   exports.getStoryById = function(req, res)
+   {
+
+   StoryModel.find({_id:req.params.id}).populate("comment").exec(function(err, story)
+   {
+      if(err){
+         res.json({code: 400, message: "Error occurred"});
+       }
+     else{
+
+     res.json({code: 200, message: "records found",data:story});
+         }
+
+     })
+   }
+     exports.updateStory = function(req, res)
+   {
+       console.log("updatestory", req.params.id, req.body);
+     //  BlogModel.findOneAndUpdate({_id: req.params.id}, { $set: req.body }, {new: true}, function(err, blog){
+
+      StoryModel.update({_id: req.params.id}, { $set: req.body }, function(err, story)
+      {
+         if(err)
+         {
+           res.json({code: 404, message: err});
+         }
+          else
+          {
+            res.json({code: 200, data:story });
+           }
+       })
+     }
+
+
+       exports.removeStory = function(req, res) {
+         console.log("removestory", req.params.id, req.body);
+         StoryModel.remove({
+           _id: req.params.id
+         }, function(err, story) {
+           if (err) {
+             res.json({
+               code: 400,
+               message: "error"
+             });
+
+           } else {
+             res.json({
+               code: 200,
+               message: "removed",
+               data: story
+             });
+
+           }
+         })
+       }
+
+       exports.addComment = function(req, res){
+         console.log(req.body);
+         var comment = new CommentModel(req.body);
+         comment.save(function(err){
+           console.log(err);
+         StoryModel.update({_id: req.params.id}, {$push: {comment: comment}}, function(err, Story){
+         if(err){ res.json({code: 404, message: err});
+         }else{
+                 res.json({code: 200, data: Story });
+               }
+           })
+         })
+       }
