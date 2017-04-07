dd1cc0c59da509514f7cc49501756eade18b82ee
hemantbisht95 <bisht.hemant99@gmail.com>
2017-04-07 17:12:56 +0530

    Update User.js

--- routes/User.js
+++ routes/User.js
@@ -1,29 +1,73 @@
 var express = require('express');
+var UserModel = require("./../app/models/User");
 var router = express.Router();
 var UserCtrl = require("./../app/controller/User");
 var StoryCtrl = require("./../app/controller/User");
+var path = require('path');
+var fs = require('fs');
+var multer  = require('multer')
+var crypto =require('crypto')
+
 /* step-4 parse token using passport and set as req.user */
 var passport = require('passport');
 var Strategy = require('passport-http-bearer').Strategy;
 var jsonwebtoken = require('jsonwebtoken');
 passport.use(new Strategy(
   function(token, callback) {
-    jsonwebtoken.verify(token, 'shhhhh', function(err, decoded) {
-      if (err) {
-        console.log(err);
+      console.log("token",token);
+      try {
+        jsonwebtoken.verify(token, 'shhhhh', function(err, decoded) {
+          if (err) {
+            // console.log("err",err);
+            callback('Invalid token');
+          } else {
+            console.log("decoded",decoded)
+            UserModel.findOne({'_id':decoded.id}).exec(function(err, user)
+              {
+                 if(user){
+                   callback(false, user);
+                     } else {
+                       console.log("err2",err)
+                           callback('Invalid token');
+                   }
+              });
+            }
+        });
+      } catch (e) {
         callback('Invalid token');
-      } else {
-          console.log(decoded)
-          callback(false, decoded);
-        }
-    });
+      }
 }));
 
+var storage = multer.diskStorage({
+  filename: function (req, file, cb) {
+    crypto.pseudoRandomBytes(16, function (err, raw) {
+      if (err) return cb(err)
 
-router.get('/getUserComment/:id',passport.authenticate('bearer', { session: false }),UserCtrl.getUserComment
-);
+      cb(null, raw.toString('hex') + Date.now() + path.extname(file.originalname))
+    })
+  }
+})
+var upload = multer({ storage: storage })
+
+// router.post('/uploadAvatar',upload.single('file'),function(req,res,next){
+//   var file = 'client/images/' + req.file.filename;
+//    fs.rename(req.file.path, file, function(err) {
+//      if (err) {
+//        console.log(err);
+//        res.sendStatus(500);
+//      } else {
+//        res.json({
+//          message: 'File uploaded successfully',
+//          filename: req.file.filename
+//        });
+//      }
+//    });
+// });
+
+router.post('/uploadAvatar',passport.authenticate('bearer', { session: false }),upload.single('file'),UserCtrl.uploadAvatar);
+router.get('/getcmnt',passport.authenticate('bearer', { session: false }),UserCtrl.getUserComment);
 router.get('/getUser',passport.authenticate('bearer', { session: false }),UserCtrl.getUser);
-router.post('/postComment',passport.authenticate('bearer', { session: false }),UserCtrl.postComment);
+router.post('/postComment/:id',UserCtrl.postComment);
 
 router.get('/userprofile',passport.authenticate('bearer', { session: false }),UserCtrl.getuserProfile);
 
@@ -33,15 +77,18 @@ router.post('/addUser', UserCtrl.addUser);
 router.post('/updateUser',passport.authenticate('bearer', { session: false }),UserCtrl.updateUser);
 router.post('/register',UserCtrl.regUser);
 router.post('/login',UserCtrl.logUser);
-router.get('/auth',passport.authenticate('bearer', { session: false }),UserCtrl.auth);
+router.get('/auth',UserCtrl.auth);
 
-router.post('/addComment/:id', StoryCtrl.addComment);
+router.post('/addComment/:id',passport.authenticate('bearer', { session: false }), StoryCtrl.addComment);
 router.get('/getStory', StoryCtrl.getStory);
 
+// router.get('/getloveStory', StoryCtrl.loveStoryStory);
 router.get('/:id',StoryCtrl.getStoryById);
 router.delete('/removeStory/:id',StoryCtrl.removeStory);
+router.get('/approvedStory/:id',StoryCtrl.approvedStory);
 router.post('/addStory', passport.authenticate('bearer', { session: false }),StoryCtrl.addStory);
 router.post('/updateStory/:id',StoryCtrl.updateStory);
 module.exports = router;
 
 module.exports = router;
+
