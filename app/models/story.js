fd269387ed6751571f794f77d48812b7bb3836da
hemantbisht95 <bisht.hemant99@gmail.com>
2017-04-07 17:10:33 +0530

    Update story.js

--- app/models/story.js
+++ app/models/story.js
@@ -1,6 +1,7 @@
 var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 var CommentModel = require("./comments");
+var UserModel = require("./User");
 ObjectId = Schema.ObjectId;
 
 
@@ -8,10 +9,34 @@ var StorySchema = new Schema({
     title: String,
     category: String,
     content:String,
+    date:{
+       type: Date,
+       default: Date.now
+     },
+    name:{
+      type:ObjectId,
+      ref:"User"
+    },
     comment:[{
       type:ObjectId,
-      ref:"comment"}]
+      ref:"comment"}],
+      isApproved:{
+          type:Boolean,
+          default:false
+        },
+    likes:String
+
 });
 
-var Story = mongoose.model('story', StorySchema)
+
+
+var deepPopulate = require('mongoose-deep-populate')(mongoose);
+StorySchema.plugin(deepPopulate, {
+  whitelist: [
+    'name',
+    'comment',
+    'comment.by'
+  ]
+});
+Story = mongoose.model('story', StorySchema)
 module.exports = Story;
