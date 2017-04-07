49a89ed659fc4997fa25bfccd77bd2547a3fc2d1
hemantbisht95 <bisht.hemant99@gmail.com>
2017-04-07 17:11:09 +0530

    Update User.js

--- app/models/User.js
+++ app/models/User.js
@@ -10,14 +10,19 @@ var Userschema = new Schema
     lastname:String,
     email:String,
     password:String,
+    created: { type: Date, default: Date.now },
     story:[{
       type:ObjectId,
       ref:"story"}],
     isAdmin:{
         type:Boolean,
         default:false
-      }
+      },
+    userprofile:String
+
 });
+
+
 
 var User = mongoose.model('User', Userschema);
 module.exports=User;
