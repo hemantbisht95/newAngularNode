ad017d908dcedb2a468ac54b6e4d1753ecab3207
hemantbisht95 <bisht.hemant99@gmail.com>
2017-04-07 17:11:37 +0530

    Update comments.js

--- app/models/comments.js
+++ app/models/comments.js
@@ -1,9 +1,14 @@
 var mongoose = require("mongoose");
+var UserModel = require("./User");
 var Schema = mongoose.Schema;
-
+var ObjectId = Schema.ObjectId;
 
 var CommentSchema = new Schema({
-  Comment: String
+  Comment: String,
+  by:{
+    type:ObjectId,
+    ref:"User"
+  }
 });
 
 var comment = mongoose.model('comment', CommentSchema)
